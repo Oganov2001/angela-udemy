af3f06724da57d80cc28510a0f6c232c4e245798
oganov2001 <georgeoganov@hotmail.com>
2022-01-19 11:13:26 +0200

    Completed the DrumKit

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -4,11 +4,13 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
-addEventListener("keypress", function (event) {
+document.addEventListener("keypress", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -45,4 +47,12 @@ function makeSound(key) {
       console.log(key);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
